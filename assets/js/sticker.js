9a90976340d87a34136768138dacc790f5a90219
MattCuadros <mcuadrose@gmail.com>
2022-11-08 23:26:14 -0300

    password.html added

--- assets/js/sticker.js
+++ assets/js/sticker.js
@@ -1,22 +1,29 @@
-const stickerLeonel=document.querySelector("#stickerLeonel");
-const stickerMarcelo=document.querySelector("#stickerMarcelo");
-const stickerJohnny=document.querySelector("#stickerJohnny");
+const stickerThor=document.querySelector("#stickerThor");
+const stickerCap=document.querySelector("#stickerCap");
+const stickerHulk=document.querySelector("#stickerHulk");
 const pMessage=document.querySelector("#pMessage");
 const btnVerify=document.querySelector("#btnVerify");
 
 btnVerify.addEventListener("click", ()=>{
-    console.log("me diste click");
-    const sticker1=+stickerLeonel.value;
-    const sticker2=+stickerMarcelo.value;
-    const sticker3=+stickerJohnny.value;
-    const sumaStickers=sticker1+sticker2+sticker3;
+    
 
-    if (sumaStickers<11){
-        pMessage.innerHTML=`Llevas ${sumaStickers} Stickers`;
+    console.log("me diste click");
+    let sticker1=+stickerThor.value;
+    let sticker2=+stickerCap.value;
+    let sticker3=+stickerHulk.value;
+    let sumaStickers=sticker1+sticker2+sticker3;
+
+    if (sticker1 === NaN || sticker2 === NaN || sticker3 === NaN || sticker1 <0 || sticker2<0 || sticker3<0){
+        alert("Debe ingresar sólo números positivos");
         return;
     }
 
-    pMessage.innerHTML=`error`;
+    if (sumaStickers<11){
+        pMessage.innerHTML=`Llevas <strong>${sumaStickers}</strong> Stickers <br><i class="fa-solid fa-circle-check"></i>`;
+        return;
+    }
+
+    pMessage.innerHTML=`Llevas demasiados Stickers. El máximo es <strong>10</strong> unidades <br><i class="fa-solid fa-circle-xmark"></i>`;
     return;
 
 
